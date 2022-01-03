08325ac0649c416a16522b6a5889bde8423fa1cd
Mohammad Sarfraz Alam <mohammadsarfrazalam@pop-os.localdomain>
2022-01-03 19:05:38 +0530

    Auth

--- pages/auth/register.js
+++ pages/auth/register.js
@@ -120,6 +120,8 @@ function Login({URL}) {
             <option value="2022">2022</option>
             <option value="2023">2023</option>
             <option value="2024">2024</option>
+            <option value="2025">2025</option>
+            <option value="2026">2026</option>
         </Field>
         </div>
         <div className="flex md:mt-5 flex-wrap flex-col md:flex-row">
