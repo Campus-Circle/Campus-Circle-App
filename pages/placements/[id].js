0f2c6bfff3d72fd3d143195ced7494a069c10668
Sarfraz <alamsarfraz422@gmail.com>
2022-04-01 20:57:19 +0530

    Placement Report

--- pages/placements/[id].js
+++ pages/placements/[id].js
@@ -1,42 +1,61 @@
-import axios from 'axios'
-import React from 'react'
-import ReactMarkdown from 'react-markdown'
-import remarkGfm from 'remark-gfm'
+import axios from "axios";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+import Loading from "../../components/Loading";
 
+function Placement({ id, data }) {
+  const router = useRouter();
 
-function Placement({id,data}) {
+  if (router.isFallback) {
     return (
-        <div className="w-full md:ml-28">
+      <div className="w-full h-screen">
+        <Loading />
+      </div>
+    );
+  }
 
-            <div className="font-body pl-5">
-                <h1 className="text-3xl py-5 text-primary border-b">
-                    Placements
-                </h1>
-                <p className="text-primary p-2 text-xl">
-                    {id}
-                </p>
-            </div>
+  return (
+    <div className="w-full md:ml-28">
+      <div className="font-body pl-5">
+        <h1 className="text-3xl py-5 text-primary border-b">Placements</h1>
+        <p className="text-primary p-2 text-xl">{id}</p>
+      </div>
 
-            <ReactMarkdown 
-            className="placement-table"
-                remarkPlugins={[remarkGfm]}
-                children={data}
-            />
-        </div>
-    )
+      <ReactMarkdown
+        className="placement-table"
+        remarkPlugins={[remarkGfm]}
+        children={data}
+      />
+    </div>
+  );
 }
 
-export default Placement
+export default Placement;
 
-export async function getServerSideProps(context) {
-    const { id } = context.query;
+export async function getStaticPaths() {
+  const { data } = await axios.get(`${process.env.URL}/placements/index.json`);
+  const paths = data.placement.map((item) => ({
+    params: {
+      id: item.name,
+    },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
 
-    const {data} = await axios.get(`${process.env.URL}/placements/${id}.md`);
-
-    return {
-        props: {
-            "data" : data,
-            "id" : id
-        }
-    }
+export async function getStaticProps({ params }) {
+  const { data } = await axios.get(
+    `${process.env.URL}/placements/${params.id}.md`
+  );
+  return {
+    props: {
+      id: params.id,
+      data: data,
+    },
+    revalidate: 1,
+  };
 }
--- pages/placements/index.js
+++ pages/placements/index.js
@@ -3,39 +3,61 @@ import React from "react";
 import Link from "next/link";
 import Head from "next/head";
 
-function Index({data, college}) {
-    return (
-        <div className="w-full h-screen flex flex-col justify-start mt-10 items-center">
-        <Head>
-          <title>{ `Placements - ${college}` }</title>
-          <meta name="og:title" content={ `Placements - ${college}` } />
-          <meta name="twitter:title" content={ `Placements - ${college}` } />
-        </Head>
-        <h1 className="text-4xl font-body font-semibold text-primary">Placements</h1>
-        <ul className="items-center font-body">
-          {data.map((item, index) => {
-            return (
-              <Link key={item.name} href={`placements/${item.name}`}>
-                <li className="tracking-tight p-3 text-xl my-2 hover:bg-primary hover:text-white font-medium cursor-pointer transition-all hover:shadow-lg px-10 rounded-md">
-                  {item.name}
-                </li>
-              </Link>
-            );
-          })}
-        </ul>
-      </div>    )
+function Index({ data, reports, college }) {
+  return (
+    <div className="w-full h-screen flex flex-col justify-start pt-10 md:ml-28">
+      <Head>
+        <title>{`Placements - ${college}`}</title>
+        <meta name="og:title" content={`Placements - ${college}`} />
+        <meta name="twitter:title" content={`Placements - ${college}`} />
+      </Head>
+      <h1 className="text-3xl font-body font-semibold text-primary">
+        Placements Records
+      </h1>
+      <ul className="items-center font-body">
+        {data.map((item, index) => {
+          return (
+            <Link key={item.name} href={`placements/${item.name}`}>
+              <li className="tracking-tight p-3 text-xl my-2 hover:bg-primary/10 text-primary font-medium cursor-pointer transition-all px-10 rounded-md w-3/5 hover:translate-x-2">
+                {item.name}
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
+
+      <hr className="border-primary border-t-4 w-5/6 opacity-30 rounded-full my-8" />
+
+      <h1 className="text-3xl font-body font-semibold text-primary py-6">
+        Placement Reports
+      </h1>
+      <ul className="items-center font-body">
+        {reports.map((item, index) => {
+          return (
+            <a href={item.link} target="_blank" key={index}>
+              <li className="tracking-tight p-3 text-xl my-2 hover:bg-primary/10 text-primary font-medium cursor-pointer transition-all px-10 rounded-md w-3/5 hover:translate-x-2 ">
+                {item.name}
+              </li>
+            </a>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
 
-export async function getServerSideProps(context) {
+export async function getStaticProps() {
   const { data } = await axios.get(`${process.env.URL}/placements/index.json`);
-    const college = process.env.COLLEGE;
-    return {
-      props: {
-        data: data,
-        college: college
-      }
-    };
-  }
-  
+  console.log(data);
+  const college = process.env.COLLEGE;
+  return {
+    props: {
+      data: data.placement,
+      reports: data.report,
+      college: college,
+    },
+    revalidate: 1,
+  };
+}
 
-export default Index
+export default Index;
